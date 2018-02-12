d99230cf16cb2a2e5f0e629c8ca0d40aaa22bf07
Michael Zheng <494649260@qq.com>
2018-02-11 19:26:05 -0500

    1

--- pb1.py
+++ pb1.py
@@ -5,7 +5,7 @@ import io
 twitter = twitter_pb2.Tweets()
 f = open("twitter.pb","rb")
 twitter.ParseFromString(f.read())
-f.close()
+
 delete_count = 0
 for tweet in twitter.tweets:
 	if tweet.is_delete==1:
@@ -19,17 +19,20 @@ for tweet in twitter.tweets:
 			reply+=1
 print("Reply Number",reply)
 
-user = {}
+u = {}
 for tweet in twitter.tweets:
 	if tweet.HasField("insert"):
 		if tweet.is_delete == 0:
 			if tweet.insert.uid in user.keys():
-				user[tweet.insert.uid]+=1
+				u[tweet.insert.uid]+=1
 			else:
-				user[tweet.insert.uid]=1
-i = 0
-for key, value in sorted(user.iteritems(), key = lambda (k,v):(v,k), reverse = True):
-	print "%s:%s"%(key, value)
-	i += 1
-	if i ==5:
-		break
+				u[tweet.insert.uid]=1
+
+for i in range(5):
+	for key, value in sorted(user.iteritems(), key = lambda (k,v):(v,k), reverse = True):
+		print "%s:%s"%(key, value)
+		i += 1
+		if i ==5:
+			break
+
+f.close()
